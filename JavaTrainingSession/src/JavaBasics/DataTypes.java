46d55fac6bec149949911ffe5f6d47581b30c9b7
dralk <dralk@192.168.0.120>
2019-07-15 07:34:17 -0700

    First commit

---
+++ JavaTrainingSession/src/JavaBasics/DataTypes.java
@@ -0,0 +1,20 @@
+package JavaBasics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10;
+		 i =20;
+		 int j=390;
+		 int k=49;
+		 
+		
+		
+		
+		
+
+	}
+
+}
